916f158feb957d44956d9f46be054b39b8c8d594
Toby <tobyeclark@gmail.com>
2024-06-08 13:01:20 +0100

    Board now tracks its width and height

--- board.py
+++ board.py
@@ -1,7 +1,11 @@
 class Board:
     board: list[list[int]] = []
+    width: int
+    height: int
 
     def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
         for i in range(width):
             self.board.append([])
             for j in range(height):
@@ -13,6 +17,6 @@ class Board:
     def get_board_value(self, x: int, y: int) -> int:
         return self.board[y][x]
 
-    def print(self):
+    def print(self) -> None:
         for row in self.board:
             print(row)
