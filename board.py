6e6124e577bb74f1b1c55ddee4b5e87ba065f4df
Toby <tobyeclark@gmail.com>
2024-06-08 13:10:28 +0100

    Fixed issue when generating the board

--- board.py
+++ board.py
@@ -6,10 +6,10 @@ class Board:
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-        for i in range(width):
+        for y in range(height):
             self.board.append([])
-            for j in range(height):
-                self.board[i].append(0)
+            for x in range(width):
+                self.board[y].append(0)
 
     def set_square_value(self, x: int, y: int, value: int) -> None:
         self.board[y][x] = value
