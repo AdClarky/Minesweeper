34986ddc7f3536bf7b0cfe40b6419f2d56902295
Toby <tobyeclark@gmail.com>
2024-06-08 13:39:22 +0100

    Renamed height and width in config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # consts
 TEXT_SIZE = 30
-X_LENGTH: int = 20
-Y_LENGTH: int = 15
-NUM_MINES: int = int((X_LENGTH * Y_LENGTH) * 0.20)  # int((X_LENGTH * Y_LENGTH) * 0.25)
+WIDTH: int = 20
+HEIGHT: int = 15
+NUM_MINES: int = int((WIDTH * HEIGHT) * 0.20)  # int((X_LENGTH * Y_LENGTH) * 0.25)
 FULLSCREEN_BOOL = False
