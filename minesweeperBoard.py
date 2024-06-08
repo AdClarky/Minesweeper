33bfc6d99f10a8ee3f32b98e5c8f7500e885d2a5
Toby <tobyeclark@gmail.com>
2024-06-08 12:35:35 +0100

    Created a basic class for a generic board

---
+++ minesweeperBoard.py
@@ -0,0 +1,18 @@
+class Board:
+    board: list[list[int]] = []
+
+    def __init__(self, width: int, height: int):
+        for i in range(width):
+            self.board.append([])
+            for j in range(height):
+                self.board[i].append(0)
+
+    def set_square_value(self, x: int, y: int, value: int) -> None:
+        self.board[y][x] = value
+
+    def get_board_value(self, x: int, y: int) -> int:
+        return self.board[y][x]
+
+    def print_board(self):
+        for row in self.board:
+            print(row)
